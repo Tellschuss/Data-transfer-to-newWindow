cd60e2e01016ac1c4c647162ec60369e76f86a24
Stewart <stewartlait@hotmail.co.uk>
2022-04-09 15:56:23 +0700

    Implement popup functionality
    When the user inputs a valid URL, the popup will display

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let popup;
+let userInputUri;
+let popupTextReceived;
+
 function OpenPopup() {
+  // 1 - assign user input to a variable
+  userInputUri = document.getElementById("txtLink").value;
+  // 2 - open the popup and pass in variable
   popup = window.open(
-    "Popup.htm",
+    userInputUri,
     "Popup",
-    "width=300,height=100",
+    "left=500,top=300,width=1000,height=600",
     "resizeble: both"
   );
 }
+
 function SendToPopup() {
   if (popup != null && !popup.closed) {
-    let lblLink = popup.document.getElementById("lblLink");
-    lblLink.innerHTML =
-      document.getElementById("txtLink").value;
+    popupTextReceived = popup.document.getElementById("popup-text-received");
+    popupTextReceived.innerHTML = userInputUri;
     popup.focus();
   } else {
     alert("Popup has been closed.");
